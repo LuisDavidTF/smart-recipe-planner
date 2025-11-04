ffaa3074ef2bfb98f718ab2b34b762ae7efcd1c5
LuisDavidTF <91746162+LuisDavidTF@users.noreply.github.com>
2025-11-04 02:29:59 -0600

    Refactor HomePage to remove API call
    Removed the getRecipes function and simplified HomePage.

--- app/page.jsx
+++ app/page.jsx
@@ -1,21 +1,6 @@
+'use client'
+
 import { RecipeFeed } from '@components/recipes/RecipeFeed';
-import { createApiClient } from '../lib/apiClient';
-
-
-async function getRecipes(limit = 6) {
-  try {
-    const api = createApiClient();
-    const data = await api.getRecipes({ limit });
-    return data;
-  } catch (error) {
-    return { data: [], nextCursor: null };
-  }
-}
-
-export default async function HomePage() {
-  const { data: initialRecipes, nextCursor } = await getRecipes(6);
-
-  return (
-    <RecipeFeed initialRecipes={initialRecipes || []} initialNextCursor={nextCursor} />
-  );
+export default function HomePage() {
+  return <RecipeFeed />;
 }
